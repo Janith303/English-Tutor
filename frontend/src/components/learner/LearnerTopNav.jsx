8b20ffc77cca93f93cae31e97bb598d65dc90854
ruwanthiThe <ruthennakoon@gmail.com>
2026-03-25 04:59:36 +0530

    commit

--- frontend/src/components/learner/LearnerTopNav.jsx
+++ frontend/src/components/learner/LearnerTopNav.jsx
@@ -11,7 +11,6 @@ export default function LearnerTopNav({ student }) {
   return (
     <nav className="bg-white border-b border-gray-100 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
-
         <img src="/logo.jpg" alt="logo" className="h-17" />
 
         <div className="hidden md:flex items-center gap-8">
@@ -34,7 +33,11 @@ export default function LearnerTopNav({ student }) {
 
         <div className="flex items-center gap-3">
           <div className="w-9 h-9 rounded-full bg-gray-200 flex items-center justify-center">
-            <img src="/avatar.png" alt="Student Avatar" className="w-full h-full object-cover rounded-full" />
+            <img
+              src="/avatar.png"
+              alt="Student Avatar"
+              className="w-full h-full object-cover rounded-full"
+            />
           </div>
         </div>
       </div>
