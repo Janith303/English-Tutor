949c594f63f869731b57fe4471ac377d54ffc53d
Dissanayaka P.G.J.S <161787933+Janith303@users.noreply.github.com>
2026-03-25 09:12:14 +0530

    fixing erro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Login from "./components/home/login";
 
-import LearnerDashboard from "./pages/LearnerDashboard";
-import PersonalizedLearningHub from "./pages/PersonalizedLearningHub";
+import LearnerDashboard from "./components/pages/LearnerDashboard";
+//import PersonalizedLearningHub from "./pages/PersonalizedLearningHub";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<LearnerDashboard />} />
-        <Route path="/hub" element={<PersonalizedLearningHub />} />
+
       </Routes>
     </Router>
   );
