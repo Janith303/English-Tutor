d4e78970dc7a7c74a2f5ac8ba262abe344314060
ruwanthiThe <ruthennakoon@gmail.com>
2026-04-21 06:40:19 +0530

    add route to admin course-approval

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -174,6 +174,7 @@ function AppRoutes() {
           <Route path="/admin/users" element={<Admin />} />
           <Route path="/admin/placement-questions" element={<Admin />} />
           <Route path="/admin/question-approval" element={<Admin />} />
+          <Route path="/admin/course-approval" element={<Admin />} />
         </Routes>
       </div>
     </div>
